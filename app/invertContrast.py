6fd7a9ad27ef2b309bb31602c94900254ede546f
solenne.vincens <solenne.vincens@icm-institute.org>
2026-04-01 17:05:59 +0200

    fixed orginalData image_series_index

--- app/invertContrast.py
+++ app/invertContrast.py
@@ -1,6 +1,5 @@
 #!/bin/python3
 
-from server.connection import Connection
 from utils.ImageFactory import ImageFactory
 from utils.check_OR_arguments import check_OR_arguments
 
@@ -15,32 +14,7 @@ import base64
 debugFolder = "/tmp/share/debug"
 
 
-def diagnostic_info(images: list, head: list, meta: list[ismrmrd.Meta]) -> None:
-    """Display diagnostic info about the images in the log"""
-
-    # Display MetaAttributes for first image
-    logging.debug("MetaAttributes[0]: %s", ismrmrd.Meta.serialize(meta[0]))
-
-    # Optional serialization of ICE MiniHeader
-    if 'IceMiniHead' in meta[0]:
-        logging.debug("IceMiniHead[0]: %s", base64.b64decode(meta[0]['IceMiniHead']).decode('utf-8'))
-
-    # Diagnostic info
-    matrix    = np.array(head[0].matrix_size  [:]) 
-    fov       = np.array(head[0].field_of_view[:])
-    voxelsize = fov/matrix
-    read_dir  = np.array(images[0].read_dir )
-    phase_dir = np.array(images[0].phase_dir)
-    slice_dir = np.array(images[0].slice_dir)
-    logging.info(f'MRD computed maxtrix [x y z] : {matrix   }')
-    logging.info(f'MRD computed fov     [x y z] : {fov      }')
-    logging.info(f'MRD computed voxel   [x y z] : {voxelsize}')
-    logging.info(f'MRD read_dir         [x y z] : {read_dir }')
-    logging.info(f'MRD phase_dir        [x y z] : {phase_dir}')
-    logging.info(f'MRD slice_dir        [x y z] : {slice_dir}')
-
-
-def process_image(images: list, config: str, metadata: str):
+def process_image(images: np.array, config: str, metadata: str):
     """Invert contrast process image"""
 
     if (len(images) == 0):
@@ -49,34 +23,12 @@ def process_image(images: list, config: str, metadata: str):
     logging.info(f'-----------------------------------------------')
     logging.info(f'     invertContrast called with {len(images)} images')
     logging.info(f'-----------------------------------------------')
-
-    # Create folder, if necessary
-    if not os.path.exists(debugFolder):
-        os.makedirs(debugFolder)
-        logging.debug("Created folder " + debugFolder + " for debug output files")
     
-    logging.debug("Processing data with %d images of type %s", len(images), images[0].data.dtype)
+    #Just changing the image_series_index of the image without any modification for test
+    images_out = []
 
-    #TO-DO:
-    #   - check_OR_arguments(config, 'InvertContrast', bool, True)
-    #   - conversion to numpy array
-    #   - diagnostic of data
-
-    # Extract image data into a 5D array of size [img cha z y x]
-    data = np.stack([img.data                              for img in images])
-    logging.info(f'MRD supposed organization : [img cha z y x]')
-    logging.info(f'MRD data shape : {data.shape}')
-    head = [img.getHead()                                  for img in images]
-    meta = [ismrmrd.Meta.deserialize(img.attribute_string) for img in images]
-
-    #display diagnostic info in the log
-    diagnostic_info(images, head, meta)
-
-    imgfactory = ImageFactory(head, meta)
-
-    data_3d = imgfactory.MRD5Dto3D(data)
-
-
-    images_out = images
+    for image in images:
+        image.image_series_index = 99
+        images_out.append(image)
 
     return images_out
--- server/pipeline/pipeline.py
+++ server/pipeline/pipeline.py
@@ -1,17 +1,25 @@
 #!/bin/python3
 
-import ismrmrd
-
-from server.connection import Connection
-
-import processing.pre_processing as pre_proc
-import processing.post_processing as post_proc
-
+import base64
+import os
 import logging
 import importlib
 
+import ismrmrd
+import numpy as np
+
+from server.connection import Connection
+
+import processing.pre_processing as pre_process
+import processing.post_processing as post_process
+
+
+
+from utils.ImageFactory import ImageFactory
 from utils.check_OR_arguments import check_OR_arguments
 
+# Folder for debug output files
+debugFolder = "/tmp/share/debug"
 
 class Pipeline:
     def __init__(self, connection: Connection, module) -> None:
@@ -20,10 +28,43 @@ class Pipeline:
         self.processor = module
         self.postprocessors = []
 
+        # Create debug folder, if necessary
+        if not os.path.exists(debugFolder):
+            os.makedirs(debugFolder)
+            logging.debug("Created folder " + debugFolder + " for debug output files")
+
     def run(self, images, config, metadata):
 
+        if (len(images) == 0):
+            return []
+        
         if check_OR_arguments(config, 'sendOriginal', bool, True) == True:
             send_original_images(images, self.connection, config, metadata)
+
+        logging.debug("Processing data with %d images of type %s", len(images), images[0].data.dtype)
+
+###########################################################################
+        #TO-DO: refactor into preprocess
+        #   (- sort the data by image type ? doing it before ?)
+        #   - check_OR_arguments(config, 'InvertContrast', bool, True)
+        #   - conversion to numpy array
+        #   - diagnostic of data
+
+        # Extract image data into a 5D array of size [img cha z y x]
+        data = np.stack([img.data                              for img in images])
+        logging.info(f'MRD supposed organization : [img cha z y x]')
+        logging.info(f'MRD data shape : {data.shape}')
+        head = [img.getHead()                                  for img in images]
+        meta = [ismrmrd.Meta.deserialize(img.attribute_string) for img in images]
+
+        #display diagnostic info in the log
+        display_diagnostic(images, head, meta)
+
+        imgfactory = ImageFactory(head, meta)
+
+        data_3d = imgfactory.MRD5Dto3D(data)
+
+###########################################################################
 
         for step in self.preprocessors:
             images = step(images, self.connection, config, metadata)
@@ -44,9 +85,6 @@ def send_original_images(images: list, connection: Connection, config: str, meta
     for image in images:
         tmpImg = image
 
-        #TO-DO: find a way to handle the image_series_index for the copy
-        tmpImg.image_series_index = 99
-
         # Ensure Keep_image_geometry is set to not reverse image orientation
         tmpMeta = ismrmrd.Meta.deserialize(tmpImg.attribute_string)
         tmpMeta['Keep_image_geometry'] = 1
@@ -55,3 +93,28 @@ def send_original_images(images: list, connection: Connection, config: str, meta
         images_copy.append(tmpImg)
         
     connection.send_image(images_copy)
+
+
+def display_diagnostic(images: list, head: list, meta: list[ismrmrd.Meta]) -> None:
+    """Display diagnostic info about the images in the log"""
+
+    # Display MetaAttributes for first image
+    logging.debug("MetaAttributes[0]: %s", ismrmrd.Meta.serialize(meta[0]))
+
+    # Optional serialization of ICE MiniHeader
+    if 'IceMiniHead' in meta[0]:
+        logging.debug("IceMiniHead[0]: %s", base64.b64decode(meta[0]['IceMiniHead']).decode('utf-8'))
+
+    # Diagnostic info
+    matrix    = np.array(head[0].matrix_size  [:]) 
+    fov       = np.array(head[0].field_of_view[:])
+    voxelsize = fov/matrix
+    read_dir  = np.array(images[0].read_dir )
+    phase_dir = np.array(images[0].phase_dir)
+    slice_dir = np.array(images[0].slice_dir)
+    logging.info(f'MRD computed maxtrix [x y z] : {matrix   }')
+    logging.info(f'MRD computed fov     [x y z] : {fov      }')
+    logging.info(f'MRD computed voxel   [x y z] : {voxelsize}')
+    logging.info(f'MRD read_dir         [x y z] : {read_dir }')
+    logging.info(f'MRD phase_dir        [x y z] : {phase_dir}')
+    logging.info(f'MRD slice_dir        [x y z] : {slice_dir}')
